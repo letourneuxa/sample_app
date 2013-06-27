9ae76fbc620cfa8d1e90dd503516723829300ad9
Antoine Letourneux <letourneuxa@yahoo.fr>
2013-06-27 13:55:54 +0200

    end chapitre 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Retourner un titre basé sur la page.
+  def titre
+    base_titre = "TutoRails"
+    if @titre.nil?
+      base_titre
+    else
+      "#{base_titre} | #{@titre}"
+    end
+  end
 end
